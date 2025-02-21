f2f74aace3167b1296cc116c970333d821432e27
Nothing <107240771+nguyenngocphung2000@users.noreply.github.com>
2025-02-21 20:50:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-
-        
-        const sections = document.querySelectorAll("section[id]");
+const sections = document.querySelectorAll("section[id]");
 
         // Add an event listener listening for scroll
         window.addEventListener("scroll", navHighlighter);
